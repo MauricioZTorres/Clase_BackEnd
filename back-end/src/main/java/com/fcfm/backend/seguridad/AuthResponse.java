450a7520455d4a350a0688d426ff27b3d19e4ea5
Mauricio Torres <zurieltorres2003@gmail.com>
2024-05-12 11:14:19 -0600

    Actividad 4.1

---
+++ back-end/src/main/java/com/fcfm/backend/seguridad/AuthResponse.java
@@ -0,0 +1,14 @@
+package com.fcfm.backend.seguridad;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class AuthResponse {
+    String token;
+}
